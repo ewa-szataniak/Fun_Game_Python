ef8844dc37923a02fcf6019f33c0bb9d35fc2562
ewa-szataniak <ewamariacoach@gmail.com>
2023-06-02 15:25:19 -0400

    weekend

---
+++ README.md
@@ -0,0 +1,5 @@
+Welcome to our Project Rock, Paper, Scissors!
+
+The Rock, Paper, Scissors project is a classic game implemented in Python. It provides an interactive and enjoyable experience for users to play the popular hand game against a computer opponent. The game follows the traditional rules, where rock beats scissors, scissors beats paper, and paper beats rock.
+
+Ewa & Gagan
--- rps.py
+++ rps.py
@@ -1,25 +1,44 @@
 import random
 
+#player's name input
+name = input("How should I address you?")
+print( "Hello " + name +"!" + " Welcome to the Rock, Paper, Scissors Game!")
 
-name = input("What is your name?")
-print( "Hello " + name +"!")
-
-#list of choices for the computer
-my_random_list = ["rock", "paper", "scissors"]
+#list of selection for the computer
+random_selection = ["rock", "paper", "scissors"]
 win_count = 0
 loss_count = 0
 tie_count = 0
 
 while True:
     #get the player's choice
-    player_choice = input ("Please choose 'rock', 'paper', or 'scissors' (enter 'quit' to exit): ").lower().strip()
-    player_choice = "".join(player_choice.split())
+    player_decision = input ("Please choose 'rock', 'paper', or 'scissors' (enter 'quit' to exit): ").lower().strip()
+    player_decision = "".join(player_decision.split())
 
-    if player_choice == "quit":
+    if player_decision == "quit":
         break
 
     #get the computer's choice
-    computer_choice = random.choice(my_random_list)
-    print("\nThe computer chose\t" + " " + computer_choice + " and you chose " + player_choice)
+    computer_pick = random.choice(random_selection)
+    print("\nThe computer chose " + computer_pick + " and you chose " + player_decision)
 
-    #Determine the winner
+    #determine the winner
+    if player_decision == computer_pick:
+        print("It's a tie! " + "You are two peas in a pod!\n")
+        tie_count += 1
+    elif (player_decision == "rock" and computer_pick == "scissors") or \
+         (player_decision == "scissors" and computer_pick == "paper") or \
+         (player_decision == "paper" and computer_pick == "rock"):
+        print("Boom! Congrats, " + name + "! "
+              "\n You won & you are smarter than computer!\n")
+        win_count += 1
+    else:
+        print("Ouch! You lost. Better luck next time!")
+        loss_count += 1
+
+
+print(f"Your current scorecard: | {str(win_count)} + Win | + {str(loss_count)} +  Loss |  + {str(tie_count)}+ Tie | \n")
+
+print("Thanks for playing, " + name + "! You are a true Ranger of Rock, Paper, Scissors Game!")
+
+
